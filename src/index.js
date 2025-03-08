02bda7c9efbb787b733937cb38b59ca546f1c32d
Bone-Structure <abbigreene13@gmail.com>
2025-03-07 22:07:07 -0500

    Inject the forecast from Html to Javascript

--- src/index.js
+++ src/index.js
@@ -49,6 +49,33 @@ function handleSearchSubmit(event){
     console.log(formatDate);
 }
 
+function displayForecast() {
+  let days = ["Tues","Wed","Thur","Fri","Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+    forecastHtml +
+    `
+    <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">☀️</div> 
+        <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+            <strong>13°</strong> 
+            </div> 
+        <div class="weather-forecast-temperature">8°</div>
+        </div>
+     </div>
+     `;
+});
+
+let forecastElement= document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+
+  }
+
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Shanghai");
+displayForecast();
